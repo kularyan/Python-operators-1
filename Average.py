63d7f72673087dc3c40a8fed44f3a4f0a5e3c568
kularyan <kularyan13@gmail.com>
2026-07-06 09:36:10 -0400

    Add files via upload

---
+++ Average.py
@@ -0,0 +1,10 @@
+# There are 5 trees in my front yard. I checked the height of each tree and found that they were different heights, which are 98, 94, 41, 95, 11
+tree_1=98
+tree_2=94
+tree_3=41   
+tree_4=95
+tree_5=11
+sum=(tree_1 + tree_2+tree_3 + tree_4 + tree_5)
+print("The sum of the heights of all the trees is:",sum)
+average = sum/5
+print("The average height of the trees is:", average)
